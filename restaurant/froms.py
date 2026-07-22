c56de2ca42e33524a7bf4c3305a683fc7d09ada6
zifoxy <thefunnylex@gmail.com>
2026-07-22 15:38:28 +0300

    Добавлены формы

---
+++ restaurant/froms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Order
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('customer_name', 'phone', 'address', 'comment')
+        widgets = {
+            'customer_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ваше имя',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '+7 (999) 123-45-67',
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Улица, дом, квартира',
+            }),
+            'comment': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Код домофона, этаж...',
+            }),
+        }
