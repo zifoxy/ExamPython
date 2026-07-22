d5eb9340df19ec1c2e06963135422ce1bd2be8c6
zifoxy <thefunnylex@gmail.com>
2026-07-22 19:58:01 +0300

    В декоратор добавлено возможность редактирования блюд для модератора

--- restaurant/decorators.py
+++ restaurant/decorators.py
@@ -1,6 +1,11 @@
 from functools import wraps
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from .models import Profile, Dish
+from django.shortcuts import get_object_or_404
+from .forms import DishForm
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
 
 def role_required(*roles):
@@ -16,3 +21,20 @@ def role_required(*roles):
         return _wrapped
     return decorator
 
+@role_required(Profile.ROLE_MODERATOR)
+def dish_edit(request, pk):
+    dish = get_object_or_404(Dish, pk=pk)
+    if request.method == 'POST':
+        form = DishForm(request.POST, request.FILES, instance=dish)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Блюдо успешно обновлено')
+            return redirect('menu')
+
+    else:
+        form = DishForm(instance=dish)
+
+    return render(request, 'restaurant/moderator/dish_form.html', {
+        'form': form,
+        'title': 'Редактировать блюдо',
+    })
