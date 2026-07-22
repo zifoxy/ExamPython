dfd0ab0c0b29eae7b53cb6d6370634843ac4dc71
zifoxy <thefunnylex@gmail.com>
2026-07-22 19:33:08 +0300

    Добавлены роли

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    ROLE_USER = 'user'
+    ROLE_MODERATOR = 'moderator'
+    ROLE_ACCOUNTANT = 'accountant'
+    ROLE_SUPPORT = 'support'
+
+    ROLE_CHOICES = [
+        (ROLE_USER, 'Пользователь'),
+        (ROLE_MODERATOR, 'Модератор'),
+        (ROLE_ACCOUNTANT, 'Бухгалтер'),
+        (ROLE_SUPPORT, 'Поддержка'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=ROLE_USER, verbose_name='Роль')
+    is_active = models.BooleanField(default=True, verbose_name='Активен')
+
+    def __str__(self):
+        return f'{self.user.username} ({self.get_role_display()})'
+
+    @property
+    def is_moderator(self):
+        return self.role == self.ROLE_MODERATOR
+
+    @property
+    def is_accountant(self):
+        return self.role == self.ROLE_ACCOUNTANT
+    
+    @property
+    def is_support(self): 
+        return self.role == self.ROLE_SUPPORT
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
