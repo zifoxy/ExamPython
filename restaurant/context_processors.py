8df8e010855e7880588bd2ca4560dce0cd88795c
zifoxy <thefunnylex@gmail.com>
2026-07-25 12:30:01 +0300

    Добавлен фукнционал в context_processor бухгалтера

--- restaurant/context_processors.py
+++ restaurant/context_processors.py
@@ -9,6 +9,7 @@ def cart(request):
 
     user_profile = None
     is_moderator = False
+    is_accountant = False
     if request.user.is_authenticated:
         try:
             user_profile = request.user.profile
@@ -16,9 +17,14 @@ def cart(request):
                 request.user.is_superuser
                 or (user_profile.is_active and user_profile.role == 'moderator')
             )
+            is_accountant = (
+                request.user.is_superuser
+                or (user_profile.is_active and user_profile.role == 'accountant')
+            )
         except ObjectDoesNotExist:
             user_profile = None
 
     context['user_profile'] = user_profile
     context['is_moderator'] = is_moderator
+    context['is_accountant'] = is_accountant
     return context
