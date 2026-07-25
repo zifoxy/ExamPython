23a0a4226ebca3c08c57eca388307d39b6e38fb7
zifoxy <thefunnylex@gmail.com>
2026-07-25 21:16:24 +0300

    Добавлен фон для сайта и отредактирован фронт под картинку

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+# https://docs.djangoproject.com/en/6.0/ref/settings/#static-files
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'restaurant' / 'static',
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
